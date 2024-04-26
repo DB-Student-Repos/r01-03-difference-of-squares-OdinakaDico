6b62ef6e7d50f9a35e5f9f666f714c28111347e7
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-26 14:28:34 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let sum = (1..=n).sum::<u32>(); 
+    let sum = n * (n + 1) / 2;
+    sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).map(|x| x * x).sum::<u32>() 
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
